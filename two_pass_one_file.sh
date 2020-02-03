df5f8e31e57ce38d01a74dbdcd616f33e38bafa0
Timothée DARCET <timothee.darcet@polytechnique.edu>
2020-02-03 08:50:15 +0100

    Use tmp dir for second script

--- two_pass_one_file.sh
+++ two_pass_one_file.sh
@@ -3,6 +3,10 @@ if [ -z $1 -o -z $4 ]; then
     exit 1
 fi
 
+temp_dir=$(mktemp -t -d -p ./)
+cd $temp_dir
+
+
 ffmpeg=~/ffmpeg-static/ffmpeg
 ffprobe=~/ffmpeg-static/ffprobe
 
@@ -15,3 +19,7 @@ echo $(pwd)
 # < /dev/null prevents ffmpeg from reading standard input
 $ffmpeg -i $input_video -codec:v libx264 -profile:v high -preset veryslow -b:v $video_bitrate -threads 0 -pass 1 -an -f mp4 -y /dev/null < /dev/null
 $ffmpeg -i $input_video -strict -2 -c:v libx264 -preset veryslow -b:v $video_bitrate -threads 0 -pass 2 -c:a aac -b:a $audio_bitrate -y $output_video < /dev/null
+
+cd ../
+rm -rf $temp_dir 
+
