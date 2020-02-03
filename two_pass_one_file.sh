661d857a3fc0faf8a957c2a9a3c6bd0e65066c91
Timothée DARCET <timothee.darcet@polytechnique.edu>
2020-02-03 02:07:54 +0100

    test

--- two_pass_one_file.sh
+++ two_pass_one_file.sh
@@ -11,5 +11,6 @@ output_video=$2
 video_bitrate=$3
 audio_bitrate=$4
 
+echo $(pwd)
 $ffmpeg -i $input_video -codec:v libx264 -profile:v high -preset veryslow -b:v $video_bitrate -threads 0 -pass 1 -an -f mp4 -y /dev/null
 $ffmpeg -i $input_video -strict -2 -c:v libx264 -preset veryslow -b:v $video_bitrate -threads 0 -pass 2 -c:a aac -b:a $audio_bitrate -y "$output_video"
