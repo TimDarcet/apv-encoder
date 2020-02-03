c5f399b75af9f960f54451d37a300941503248cb
Timothée DARCET <timothee.darcet@polytechnique.edu>
2020-02-03 01:51:19 +0100

    Distribute

--- encode.sh
+++ encode.sh
@@ -89,8 +89,8 @@
          video_bitrate=$(echo "($desired_size*$constant_quality_bitrate*$coef)/$total_size_coeffed-$audio_bitrate" | bc )
          #two-pass encode
          #TODO ACTUAL PATH OF SECOND SCRIPT
-         #ssh -oStrictHostKeyChecking=no $(head -n1 ./computers_name.tmp) "./two_pass_one_file.sh \"$video\" \"$folder_to_encode/encoding_final_output/$foldername/$filename\" $video_bitrate $audio_bitrate" &
-         ../two_pass_one_file.sh "$video" "$folder_to_encode/encoding_final_output/$foldername/$filename" $video_bitrate $audio_bitrate
+         ssh -oStrictHostKeyChecking=no $(head -n1 ./computers_name.tmp) "./two_pass_one_file.sh \"$video\" \"$folder_to_encode/encoding_final_output/$foldername/$filename\" $video_bitrate $audio_bitrate" &
+         #../two_pass_one_file.sh "$video" "$folder_to_encode/encoding_final_output/$foldername/$filename" $video_bitrate $audio_bitrate
          sed -i '1d' ./computers_name.tmp
          #$ffmpeg -i "$video" -codec:v libx264 -profile:v high -preset veryslow -b:v $video_bitrate -threads 0 -pass 1 -an -f mp4 -y /dev/null
          #$ffmpeg -i "$video" -strict -2 -c:v libx264 -preset veryslow -b:v $video_bitrate -threads 0 -pass 2 -c:a aac -b:a $audio_bitrate -y "$folder_to_encode/encoding_final_output/$foldername/$filename"
