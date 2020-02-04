346340a713413922b8c7709dcb3cbdd426931022
Timothée DARCET <timothee.darcet@polytechnique.edu>
2020-02-04 02:25:58 +0100

    Use computers as filenames for locks

--- encode.sh
+++ encode.sh
@@ -44,7 +44,8 @@
      mkdir -p "$folder_to_encode/../constant_quality_output/$foldername"
      filename=$(basename $video)
      mkdir ../locks
-     ssh -oStrictHostKeyChecking=no $(head -n1 ./computers_name.tmp) "cd $(dirname $(pwd)) && touch ./locks/$filename && $ffmpeg -i $(dirname $(pwd))/${video#../} -c:v libx264 -preset medium -crf $video_quality_factor -pix_fmt yuv420p -threads 0 -c:a copy -y ${folder_to_encode#../}/../constant_quality_output/$foldername/$filename > ./locks/$filename && rm -f ./locks/$filename" &
+     computer=$(head -n1 ./computers_name.tmp)
+     ssh -oStrictHostKeyChecking=no $computer "cd $(dirname $(pwd)) && touch ./locks/$computer && $ffmpeg -i $(dirname $(pwd))/${video#../} -c:v libx264 -preset medium -crf $video_quality_factor -pix_fmt yuv420p -threads 0 -c:a copy -y ${folder_to_encode#../}/../constant_quality_output/$foldername/$filename > ./locks/$computer && rm -f ./locks/$computer" &
      printf "[%s] encodage n°1 de %s lancé sur %s.\n" $(date +%H:%M:%S) $video $(head -n1 ./computers_name.tmp)
      sed -i '1d' ./computers_name.tmp
  done 
