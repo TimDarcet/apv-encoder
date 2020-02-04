05047d10db33fb5e2b0dfa36b335f18d6f856aea
Timothée Darcet <timothee.darcet+git@m4x.org>
2020-02-04 22:42:58 +0100

    Search for coef file recursively upwards

--- encode.py
+++ encode.py
@@ -125,7 +125,10 @@ def encode(folder_to_encode, target_size, computers_file):
     for video in folder_to_encode.rglob('*.mp4'):
         out_file = output_1_folder / video.relative_to(folder_to_encode)
         # Read coef
-        coef = int((video.parent / '.coef').read_text().strip())
+        coefpath = video.parent / '.coef'
+        while not coefpath.is_file():
+            coefpath = coefpath.parent.parent / '.coef'
+        coef = int(coefpath.read_text().strip())
         # Check if the encoding 1 worked
         if (not out_file.is_file()):
             raise ValueError("Could not find output of first encoding for {}"\
