850d4007fc0d40defa77b45ee322d548a8c315df
Timothée DARCET <timothee.darcet@polytechnique.edu>
2020-02-04 02:06:49 +0100

    More robust iteration on files

--- batch_rename.sh
+++ batch_rename.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-# Rename all files in $1/* to the format for the encoding script
+# Rename all .mp4 files in $1 (max depth 3) to the format for the encoding script
 # $1 : folder to rename
 # $2 : bitrate coef to give
 
-for i in $1/*/*
+for i in $(find "$1" -maxdepth 3 -type f -name "*.mp4" | sort )
 do
 mv "$i" "${i%.*}_$2.${i##*.}"
 done
