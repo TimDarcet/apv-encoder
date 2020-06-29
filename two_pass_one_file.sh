7c4d39cdc6e627011b9f868461b2719de1c31ad5
Titus Senez <titus.senez@polytechnique.edu>
2020-06-29 21:22:04 +0200

    add movflags faststart to comply with browser reading

--- two_pass_one_file.sh
+++ two_pass_one_file.sh
@@ -16,8 +16,8 @@ ffprobe=$6
 
 echo $(pwd)
 # < /dev/null prevents ffmpeg from reading standard input
-$ffmpeg -i "$input_video" -loglevel error -hide_banner -codec:v libx264 -profile:v high -preset veryslow -b:v $video_bitrate -minrate 400000 -threads 0 -pass 1 -an -f mp4 -y /dev/null < /dev/null
-$ffmpeg -i "$input_video" -loglevel error -hide_banner -strict -2 -c:v libx264 -preset veryslow -b:v $video_bitrate -minrate 400000 -threads 0 -pass 2 -c:a aac -b:a $audio_bitrate -y "$output_video" < /dev/null
+$ffmpeg -i "$input_video" -loglevel error -hide_banner -codec:v libx264 -profile:v high -preset veryslow -movflags faststart -b:v $video_bitrate -minrate 400000 -threads 0 -pass 1 -an -f mp4 -y /dev/null < /dev/null
+$ffmpeg -i "$input_video" -loglevel error -hide_banner -strict -2 -c:v libx264 -preset veryslow -movflags faststart -b:v $video_bitrate -minrate 400000 -threads 0 -pass 2 -c:a aac -b:a $audio_bitrate -y "$output_video" < /dev/null
 
 cd ../
 rm -rf $temp_dir
